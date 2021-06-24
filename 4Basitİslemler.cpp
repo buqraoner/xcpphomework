c908fd301411ba4182a329f1c4836496777de1e3
buqraoner <83416454+buqraoner@users.noreply.github.com>
2021-06-24 20:19:14 +0300

    Add files via upload

---
+++ 4Basitİslemler.cpp
@@ -0,0 +1,26 @@
+//Ekrandan okunan iki tam sayý (int) için 
+// basit iþlemler yaparak ekrana sonuçlarý basan kod yazýnýz. 
+//Bu iþlemler toplama, çýkarma, çarpma, bölme ve kalan iþlemleridir.
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Iki adet  sayi giriniz";
+	float sayi1,sayi2;
+	cin >> sayi1>>sayi2;
+	
+	cout <<sayi1+sayi2<<endl
+	<<sayi1-sayi2<<endl
+	<<sayi1/sayi2<<endl
+	<<sayi1*sayi2;
+	
+	
+	
+
+	
+	
+	
+}
