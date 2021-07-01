bbf27e7991e01dccd330ca90897dcf6410972b7a
buqraoner <83416454+buqraoner@users.noreply.github.com>
2021-07-01 13:53:54 +0300

    Add files via upload

---
+++ 5logic.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int sayi1,sayi2;
+	cout <<"Enter two numbers"<<endl;
+	cin >>sayi1>>sayi2;
+
+	cout <<"Girdiginiz sayilar "<<endl<<sayi1<<endl<<sayi2<<endl;
+	
+	
+	
+	    if (sayi1 == sayi2)
+            {
+                cout<<"Sayilar esitdir"<<endl;
+            }
+            else
+            {
+                cout<<"Sayilar esit degildir"<<endl;
+            }
+            if (sayi1 != sayi2)
+            {
+                 cout<<"Sayilar farklidir"<<endl;
+            }
+            else
+            {
+                cout<<"Sayilar farkli degildir"<<endl;
+            }
+            if (sayi1 < sayi2)
+            {
+                 cout<<"Birinci sayi ikinci sayidan kucukdur"<<endl;
+            }
+            else
+            {
+                 cout<<"Birinci sayi ikinci sayidan kucuk degildir"<<endl;
+            }
+        
+        }
+
+   
+
+
