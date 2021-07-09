744e846939b3ed7e3f65a484450af54c2846d6b0
buqraoner <83416454+buqraoner@users.noreply.github.com>
2021-07-09 16:38:41 +0300

    Add files via upload

---
+++ Döngü9.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// 1'den 10'a kadar dongu
+int main()
+{
+
+
+int i;
+int z;
+for(i = 1; i<=10; i++)
+{ 
+
+cout <<i	
+	 <<"\t";
+
+	
+	
+	
+	
+
+	}	
+	
+	
+	
+	
+	
+	
+}
