836977fbf8bc47dd02528710efab3b00edd0b863
buqraoner <83416454+buqraoner@users.noreply.github.com>
2021-07-15 19:09:54 +0300

    Add files via upload

---
+++ 13CarpimTablosu.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+    int main(){
+   for (int carpan = 1; carpan < 11; carpan++) {
+     for (int carpilan = 1; carpilan < 11; carpilan++) {
+       cout << carpan << " x " << carpilan << " = " << 
+                      carpan * carpilan << endl;
+   }
+     cout << endl;
+	 
+}
+
+
+
+return 0;
+
+
+	    }
