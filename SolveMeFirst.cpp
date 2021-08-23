7d5b1fcf5c267f0a1ab2796d42c574ce70455a4f
buqraoner <83416454+buqraoner@users.noreply.github.com>
2021-08-23 16:06:25 +0300

    Add files via upload

---
+++ SolveMeFirst.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+/*
+omplete the function solveMeFirst to compute the sum of two integers.
+
+Example
+
+
+Return .
+
+Function Description
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of  and 
+
+Constraints
+
+
+Sample Input
+
+a = 2
+b = 3
+Sample Output
+
+5
+Explanation
+
+.  
+*/
+
+int solveMeFirst(int a, int b )
+	{
+		cout<<a+b;
+	}
+int main()
+{
+	int x,y;
+	cout <<"First Number : "; cin>>x;
+	cout <<"Second Number : "; cin >>y;
+	
+	solveMeFirst(x,y);
+	
+
+	
+}
+
